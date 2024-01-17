53d07a0e50b09c2fa4dba7569340deb6d99f2dab
Dmytro-A-galaxy-it <dmytro.a@galaxy-it.net>
2024-01-17 17:08:29 +0300

    Create project Opera-211

---
+++ system/storage/cache/7d/7d72f631926812f47c21b1d25fcdb4066426c7496cbdb4867c22aae559f8689a.php
@@ -0,0 +1,52 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_1964e7f57caa67069d3c6ed84ae4b2ef4d371aff1e2d8caf7389c385d0e90a85 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\"><a href=\"http://www.opencart.com\" target=\"_blank\">";
+        // line 2
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>|<a href=\"http://www.opencart.com/index.php?route=documentation/introduction\" target=\"_blank\">";
+        echo (isset($context["text_documentation"]) ? $context["text_documentation"] : null);
+        echo "</a>|<a href=\"http://forum.opencart.com\" target=\"_blank\">";
+        echo (isset($context["text_support"]) ? $context["text_support"] : null);
+        echo "</a><br />
+    </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container"><a href="http://www.opencart.com" target="_blank">{{ text_project }}</a>|<a href="http://www.opencart.com/index.php?route=documentation/introduction" target="_blank">{{ text_documentation }}</a>|<a href="http://forum.opencart.com" target="_blank">{{ text_support }}</a><br />*/
+/*     </div>*/
+/* </footer>*/
+/* </body></html>*/
